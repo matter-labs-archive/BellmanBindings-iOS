a0319a4ac29ec45a6845d7450a333a87fad1ee07
BaldyAsh <antongrigorjev2010@gmail.com>
2019-03-28 15:11:41 +0300

    added header for test

---
+++ cargo/src/test/test.h
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef enum {
+  Bls12,
+  Bn256,
+} EngineType;
+
+typedef struct {
+  bool value;
+  char *error;
+} VerificationResult;
+
+VerificationResult test_verify(const char *file_with_vk,
+                               const uint8_t *inputs_array,
+                               uintptr_t inputs_array_size,
+                               EngineType engine,
+                               const uint8_t *proof_vec,
+                               uintptr_t proof_vec_size);
